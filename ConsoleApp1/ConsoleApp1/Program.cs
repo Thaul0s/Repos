8a4fc81df98047e2cc6d59f7aa1c1d7fa9effa21
Angelovski Dimitar <dimitar.angelovski@hotmail.com>
2019-12-27 20:32:40 +0100

    OOO TI

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello");
             Console.WriteLine("Evo ti ga na");
+            Console.WriteLine("OOOOOOOOOOJ NOOONEN");
         }
     }
 }
