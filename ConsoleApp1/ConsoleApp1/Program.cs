ec8f234741e5f0d40a3ff2cc52943da2f9ef8aa8
Pavle Buzaroski <pavlebuzaroski@outlook.com>
2019-12-27 20:41:28 +0100

    Tes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello");
             Console.WriteLine("Evo ti ga na");
             Console.WriteLine("OOOOOOOOOOJ NOOONEN");
+            double angele;
         }
     }
 }
